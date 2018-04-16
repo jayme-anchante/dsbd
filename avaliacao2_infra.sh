be564371e8f11660586fe514f5006129887dca74
Jayme Anchante <jt_anchante@hotmail.com>
2018-04-15 22:13:39 -0300

    Update avaliacao2_infra.sh

--- avaliacao2_infra.sh
+++ avaliacao2_infra.sh
@@ -9,26 +9,34 @@ ext2=$4
 dir2=$5
 
 if [ -d "$dir1" ]; then
+# check whether directory $dir1 exists
 
     for file in $dir1*.$ext1; do
+    # iterate over files in directory $dir1 with given extension $ext1
 
         if [ -r "$file" ]; then
+        # check whether user has reading permission over the file
 
             fileName=$(basename $file .$ext1)
+            # retrieve only the file name (without the path or the extension)
 
             if [ "$param" == "-m" ]; then
+            # check whether mv operation
 
                 echo "Changing file $dir1$fileName.$ext1 to $fileName.$ext2"
                 mv $dir1$fileName.$ext1 $dir1$fileName.$ext2
 
             elif [ "$param" == "-c" ]; then
+            # check whether cp operation
 
                 if [ $# == 4 ]; then
+                # checke whether rename in the same folder
 
                     echo "Renaming file $dir1$fileName.$ext1 to $dir1$fileName.$ext2"
                     cp $dir1$fileName.$ext1 $dir1$fileName.$ext2
 
                 elif [ $# == 5 ]; then
+                # check whether rename and copy to another fodler
 
                     echo "Copying and renaming file from $dir1$fileName.$ext1 to $dir2$fileName.$ext2"
                     cp $dir1$fileName.$ext1 $dir2$fileName.$ext2
